761cde8c3e43ed7d33a4fb1f64181a659884e914
Aditya <aditya@192.168.1.5>
2024-07-25 19:54:33 +0530

    adding changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Button = ({ variant = "solid", children, ...props }) => {
-  const baseClasses = "px-4 py-2 font-semibold rounded-3xl";
+  const baseClasses = "px-4 py-2 font-semibold rounded-3xl mr-2 ";
 
   const variantClasses =
     variant === "outlined"
-      ? "border-2 h-12 border-black"
-      : "bg-black h-12 text-white";
+      ? "border-2 h-14 border-black"
+      : "bg-black h-14 text-white";
 
   return (
     <button className={`${baseClasses} ${variantClasses}`} {...props}>
